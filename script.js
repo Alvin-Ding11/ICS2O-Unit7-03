f1874615d5650ac856a7c3c403993a16dbf9c95e
AlvinDing11 <alvin.ding@ocsbstudent.ca>
2021-03-24 13:17:33 +0000

    js fixes

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function lines () {
   } else if (age >= 5) {
     document.getElementById('line1').innerHTML = 'You can see a G or PG movie alone.'
   } else {
-    "Uh. You're too young for most things."
+    document.getElementById('line1').innerHTML = "Uh. You're too young for most things."
   }
 
   document.getElementById('line2').innerHTML = 'Thank you for verifying your age!'
